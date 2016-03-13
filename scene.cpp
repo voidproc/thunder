239a5de2399fe0befb064fd910449ea57f1003ce
voidproc <voidproc@gmail.com>
2016-03-13 10:39:35 +0900

    到達WAVEが保存されていなかったのを修正

--- scene.cpp
+++ scene.cpp
@@ -301,6 +301,7 @@ void SceneMain::update()
             updateHiscore();
         }
         if (wave_ > waveMax_) {
+            waveMax_ = wave_;
             updateWaveMax();
         }
         addScene(new SceneGameOver(score_));
@@ -313,6 +314,7 @@ void SceneMain::update()
                 updateHiscore();
             }
             if (wave_ > waveMax_) {
+                waveMax_ = wave_;
                 updateWaveMax();
             }
             addScene(new SceneClear(score_));
